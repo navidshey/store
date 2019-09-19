f4157db9cda759c847f676c3b9a70785e74bb8e7
navid <sheydaein@gmail.com>
2019-09-19 20:10:29 +0430

    add fake db and faker.js

---
+++ api/workers.js
@@ -0,0 +1,41 @@
+let faker = require('faker');
+
+//git repo
+//https://github.com/marak/Faker.js/
+
+let generateWorkers = () => {
+    let workers = [];
+    let tutorials =[];
+
+    for (let id = 0; id < 30; id++) {
+        let firstName = faker.name.firstName();
+        let lastName = faker.name.lastName();
+        let email = faker.internet.email();
+
+        workers.push({
+            "id": id,
+            "firstName": firstName,
+            "lastName": lastName,
+            "email": email
+        });
+    }
+    
+
+    
+    for (let id = 0; id < 30; id++) {
+        let title = faker.name.title();
+        let description = faker.name.jobDescriptor()
+        let tutor = faker.name.lastName;
+
+        tutorials.push({
+            "id": id,
+            "title": title,
+            "description": description,
+            "tutor": tutor
+        });
+    }
+
+    return { "workers": workers, "tutorials": tutorials }
+}
+
+module.exports = generateWorkers
